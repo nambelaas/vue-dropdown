e091b6d808db3c63b9df899b47b2700d24f6da82
nambelaas <salmaniseif@gmail.com>
2026-05-09 13:57:31 +0700

    add vue dropdown

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<script lang="ts">
+import Dropdown from './components/Dropdown.vue'
+export default {
+  name: 'App',
+  components: {
+    Dropdown,
+  },
+}
+</script>
+
+<template>
+  <Dropdown>
+    <template #trigger>
+      <button
+        id="dropdownDefaultButton"
+        data-dropdown-toggle="dropdown"
+        class="bg-blue-500 rounded-lg cursor-pointer outline inline-flex items-center justify-center text-white box-border border border-transparent hover:bg-blue-600 focus:ring-4 focus:ring-blue-300 shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none"
+        type="button"
+      >
+        Dropdown button
+        <svg
+          class="w-4 h-4 ms-1.5 -me-0.5"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="m19 9-7 7-7-7"
+          />
+        </svg>
+      </button>
+    </template>
+    <template #menu>
+      <li>
+        <a
+          href="#"
+          class="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded"
+          >Dashboard</a
+        >
+      </li>
+      <li>
+        <a
+          href="#"
+          class="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded"
+          >Settings</a
+        >
+      </li>
+      <li>
+        <a
+          href="#"
+          class="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded"
+          >Earnings</a
+        >
+      </li>
+      <li>
+        <a
+          href="#"
+          class="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded"
+          >Sign out</a
+        >
+      </li>
+    </template>
+  </Dropdown>
+</template>
---
+++ src/components/Dropdown.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="dropdown">
+    <slot name="trigger"></slot>
+    <!-- Dropdown menu -->
+    <div
+      id="dropdown"
+      class="z-10 bg-neutral-primary-medium border border-[0.5px] border-gray-200 rounded-lg shadow-sm w-44 mt-1"
+    >
+      <ul class="p-2 text-sm text-body font-medium" aria-labelledby="dropdownDefaultButton">
+        
+
+        <slot name="menu"></slot>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Dropdown',
+}
+</script>
